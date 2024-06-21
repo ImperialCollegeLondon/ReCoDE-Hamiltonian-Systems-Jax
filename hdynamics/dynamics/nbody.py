ae7d978571e47a06bac8543fe0e17a1c7f891a3a
Tycho van der Ouderaa <tychovdo@gmail.com>
2024-06-21 09:28:46 +0100

    ruff reformat

--- hdynamics/dynamics/nbody.py
+++ hdynamics/dynamics/nbody.py
@@ -34,7 +34,7 @@ class Nbody(Dynamics):
 
         q, p = x.reshape(2, self.n_bodies, self.dim)
 
-        H_kinetic = jnp.sum((jnp.linalg.norm(p, axis=1) ** 2) / (2 * (masses ** 2)))
+        H_kinetic = jnp.sum((jnp.linalg.norm(p, axis=1) ** 2) / (2 * (masses**2)))
 
         q_dists = q.reshape(self.n_bodies, 1, self.dim) - q.reshape(
             1, self.n_bodies, self.dim
