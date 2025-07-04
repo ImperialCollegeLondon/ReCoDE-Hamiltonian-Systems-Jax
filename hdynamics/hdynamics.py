6832b06fadb70a9b8f536321c1461a5c9f8ded70
Chris Cooling <cmcooling@gmail.com>
2025-07-04 09:16:49 +0100

    Removed unused base class method

--- hdynamics/hdynamics.py
+++ hdynamics/hdynamics.py
@@ -5,6 +5,9 @@ from abc import ABC, abstractmethod
 import jax
 import jax.numpy as jnp
 
+from hdynamics.odeint import ode_int
+from hdynamics.utils import symplectic_form
+
 
 class Dynamics(ABC):
     """Abstract class for a dynamical system."""
@@ -33,10 +36,42 @@ class Dynamics(ABC):
 
         return x_start
 
+    def jac_h(self):
+        """Return the gradient of the Hamiltonian with respect to x."""
+        return jax.grad(self.H)
+
+    def get_rate_of_change(self):
+        """Return a function grad_x(x, t) suitable for ODE integration."""
+        jac_h = self.jac_h()
+
+        def grad_x(x, t):
+            return symplectic_form(jac_h(x))
+
+        return grad_x
+
+    def generate_trajectory(self, x_start, stepsize, n_steps=500):
+        """Simulate a single trajectory using the provided gradient function and initial state.
+
+        Args:
+            x_start (array-like): Initial state vector.
+            stepsize (float): Step size for integration.
+            n_steps (int, optional): Number of time steps.
+
+        Returns:
+            solution (jax.numpy.ndarray): Simulated trajectory of shape (n_steps+1, M).
+            t_span (jax.numpy.ndarray): Time points for the simulation.
+        """
+        t_start = 0.0
+        t_end = n_steps * stepsize
+
+        t_span = jnp.linspace(t_start, t_end, n_steps + 1)
+
+        grad_x = self.get_rate_of_change()
+
+        solution = ode_int(grad_x, x_start, t_span, atol=1e-10, rtol=1e-10)
+
+        return solution, t_span
+
     def plot_trajectory(self, trajectory, t_span, ax):
         """Plot trajectory within specified t_span range."""
-        raise NotImplementedError("No trajectory plotting available for selected dynamical system.")
-
-    def plot_H(self, ax):
-        """Plot the Hamiltonian."""
-        raise NotImplementedError("No energy plotting available for selected dynamical system.")
+        raise NotImplementedError("No trajectory plotting available for this dynamical system.")
